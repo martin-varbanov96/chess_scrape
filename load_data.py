913c641455400322c84a99b4f1484c43e8da0c95
Martin Varbanov <martin_varbanov96@abv.bg>
2021-02-14 11:50:24 +0200

    added mongo load option

---
+++ load_data.py
@@ -0,0 +1,19 @@
+# load_data.py
+from PersonalVariables import personal_variables as pv
+import pymongo
+
+class DataLoader:
+    def __init__(self):
+        self.mongo_client = pymongo.MongoClient(pv.mongo_connection)
+        self.db = self.mongo_client[pv.cloud_mongo_db]
+
+    def mongo_many_insert_to_collection(self, collection, docs_list):
+        assert(type(docs_list) == list)
+        assert(type(collection) == str)
+        col = self.db[collection]
+        status = col.insert_many(docs_list)
+
+        # TODO: should it be more assertive?
+        if(status == []):
+            print("Nothing was inserted")
+
