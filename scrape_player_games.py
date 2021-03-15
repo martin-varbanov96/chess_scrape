7f13cd254bf8aa8371db37da1ca4872d376464aa
Martin Varbanov <martin_varbanov96@abv.bg>
2021-03-15 16:31:06 +0200

    added functionality to scrape last month's games

--- scrape_player_games.py
+++ scrape_player_games.py
@@ -1,13 +1,24 @@
 from fetch_data import DataFetcher
 from load_data import DataLoader
+import datetime
 
 NAME = "funvengeance"
 CHESS_GAMES = "chess_games"
 
-# get games
-data_fetcher = DataFetcher(NAME)
-json_games_array = data_fetcher.get_array_chess_games("01", 2021)
+class PlayerGamesScraper():
 
-# insert games
-data_loader = DataLoader()
-data_loader.mongo_many_insert_to_collection(CHESS_GAMES, json_games_array)
+    def scrape_last_month_games(self, player_name):
+        # get data for last month
+        today = datetime.date.today()
+        first = today.replace(day=1)
+        lastMonth = first - datetime.timedelta(days=1)
+        last_month_str = lastMonth.strftime("%m")
+        las_month_year_int = int(lastMonth.strftime("%Y"))
+        
+        # get games
+        data_fetcher = DataFetcher(player_name)
+        json_games_array = data_fetcher.get_array_chess_games(last_month_str, las_month_year_int)
+
+        # insert games
+        data_loader = DataLoader()
+        data_loader.mongo_many_insert_to_collection(CHESS_GAMES, json_games_array)
