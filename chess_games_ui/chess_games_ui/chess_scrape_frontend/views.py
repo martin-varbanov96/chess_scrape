2d87397735502e962a89b68855d17b2a2f67f5cd
Martin Varbanov <martin_varbanov96@abv.bg>
2021-06-13 11:00:31 +0300

    added 404 page and logic for games to show in table

--- chess_games_ui/chess_games_ui/chess_scrape_frontend/views.py
+++ chess_games_ui/chess_games_ui/chess_scrape_frontend/views.py
@@ -1,10 +1,28 @@
+from all_chess_games.models import ChessGame
 from django.http import HttpResponse
-from django.template import loader
+from django.template import Context, loader, Template
 from django.shortcuts import render
 
 def about_page(request):
     return render(request, 'about_page/about_page.html')
 
 def all_chess_games(request):
-    return render(request, 'all_games/all_chess_games.html')
+    all_chess_games_html = 'all_games/all_chess_games.html'
+    all_chess_games = ChessGame.objects.all()
+    all_chess_games_size = len(all_chess_games)
+    uri = str(request.path)
+    page_number = int(uri.split("/")[2])
+
+    if(page_number*20 > all_chess_games_size):
+        # TODO: give 404 page
+        print("*"*300)
+        return render(request, "chess_scrape_frontend/404_page.html")
+    else:
+        games_to_show = all_chess_games[page_number*20:page_number*20+20]
+        
+        context = {"games_to_show": games_to_show}
+
+        return render(request,
+                    all_chess_games_html,
+                    context)
 
